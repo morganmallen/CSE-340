438bfceb1d6b632090c25498600e168db992820a
Morgan Allen <morganallen1999@gmail.com>
2025-01-13 15:22:45 -0700

    week 2 done

---
+++ database/assignment2.sql
@@ -0,0 +1,44 @@
+--1
+INSERT INTO public.account (
+account_firstname, 
+account_lastname, 
+account_email, 
+account_password
+)
+VALUES (
+'Tony',
+'Stark',
+'tony@starkent.com',
+'Iam1ronM@n'
+);
+
+--2
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+--3
+DELETE FROM account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark'
+
+--4
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer'
+
+--5
+SELECT
+	inv_make,
+	inv_model,
+	classification_name
+FROM 
+	inventory
+INNER JOIN classification
+	ON inventory.classification_id = classification.classification_id
+WHERE classification_name = 'Sport'
+
+--6
+UPDATE inventory
+SET 
+	inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
